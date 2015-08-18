c55d47a8bbfb9e00f8eb9457f32b29dd73a56b2e
tkasparaitis <tomas@mediaart.lt>
2015-08-18 18:14:24 +0300

    small change

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var WebToPay = require('./WebToPay.js');
-var f = new WebToPay({ 'projectId' : '40712', 'signPassword' : '3f722c39358fed674747c4197d6d6687' });
+var f = new WebToPay({ 'projectId' : 'XXXXX', 'signPassword' : 'YYYYYYYYYYYY' });
 
 f.orderid = '123';
 f.accepturl = 'http://test.lt';
 f.cancelurl = 'http://test.lt';
 f.callbackurl = 'http://test.lt';
-f.p_email = 't2@evp.lt';
+f.p_email = 'test@test.com';
 f.amount = 100;
 f.currency = 'EUR';
 
